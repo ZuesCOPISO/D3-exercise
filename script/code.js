bcfd85e5b6d175fdd864f634357588b2ebe8a087
ZeusCOPISO <copisozizipho2@gmail.com>
2024-05-17 16:28:22 +0200

    v1

---
+++ script/code.js
@@ -0,0 +1,19 @@
+// Declare variables for name and surname
+var firstName = "Zeus";
+var lastName = "Copiso";
+
+// Concatenate the variables
+var fullName = firstName + " " + lastName;
+console.log(fullName); 
+
+// variable for numberVar
+var numberVar = 5;
+
+// Multiply numberVar by 5
+console.log(numberVar * 5); // Output: 25
+
+// Create a constant with the value of your name
+const myName = "Zeus";
+
+// Try to reassign the value with your surname
+myName = "Copiso"; // This will result in an error because constants cannot be reassigned
